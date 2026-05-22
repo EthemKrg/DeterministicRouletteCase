7c3c8b646cfa67ccf1c9b9acf89124158c411230
İbrahim Ethem Kurugöl <ibrahimethemkurugol@gmail.com>
2026-05-22 14:08:14 +0300

    add basic bet model

---
+++ Assets/_Project/Scripts/Betting/BetType.cs
@@ -0,0 +1,17 @@
+public enum BetType
+{
+    Straight,
+    Split,
+    Street,
+    Corner,
+    SixLine,
+
+    Red,
+    Black,
+    Even,
+    Odd,
+    Low,
+    High,
+    Dozen,
+    Column
+}
